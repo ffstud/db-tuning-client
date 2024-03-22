3918b0cd0d995ad368eb43c43c3790e45e91d345
compilerbeats <peter.lindner.sbg@gmail.com>
2024-03-22 21:25:18 +0100

    finalized the first assignment

--- src/edu/plus/cs/Main.java
+++ src/edu/plus/cs/Main.java
@@ -1,6 +1,7 @@
 package edu.plus.cs;
 
 import edu.plus.cs.assignment.Assignment1;
+import edu.plus.cs.util.AssignmentType;
 import edu.plus.cs.util.DbUtils;
 
 import java.sql.*;
@@ -8,15 +9,26 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Optional<Connection> connection = DbUtils.connectToMariaDb();
+        if (args.length < 1) {
+            System.err.println("Invalid call, no assignment given");
+        }
 
-        if (connection.isPresent()) {
-            System.out.println("------- Assignment 1 -------");
-            Assignment1.executeAssignment1(connection.get());
-        } else {
-            System.err.println("Could not execute assignment 1: no database connection present!");
+        AssignmentType assignmentType = null;
+        try {
+            assignmentType = AssignmentType.valueOf(args[0].toUpperCase());
+
+            switch (assignmentType) {
+                case A1 -> {
+                    System.out.println("------- Assignment 1 -------");
+                    Assignment1.executeAssignment1();
+                }
+                default -> {
+                    System.err.println("Invalid assignment provided: " + args[0]);
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Could not identify correct assignment type for: " + args[0]);
         }
     }
-
-
 }
--- src/edu/plus/cs/assignment/Assignment1.java
+++ src/edu/plus/cs/assignment/Assignment1.java
@@ -6,11 +6,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.Optional;
 
 public class Assignment1 {
-    public static void executeAssignment1(Connection connection) {
+
+    public static void executeAssignment1() {
         String inputFile = "auth.tsv";
 
+        // POSTGRES
+        executeOnPostgres(inputFile);
+
+        // MARIA DB
+        executeOnMariaDb(inputFile);
+    }
+
+    private static void executeOnPostgres(String inputFile) {
+        Optional<Connection> optionalConnection = DbUtils.connectToPostgres();
+
+        if (optionalConnection.isEmpty()) {
+            System.err.println("Could not establish connection to postgres");
+            return;
+        }
+
+        Connection connection = optionalConnection.get();
         long startTimestamp;
         long endTimestamp;
         try {
@@ -52,6 +70,57 @@ public class Assignment1 {
         }
     }
 
+    private static void executeOnMariaDb(String inputFile) {
+        Optional<Connection> optionalConnection = DbUtils.connectToMariaDb();
+
+        if (optionalConnection.isEmpty()) {
+            System.err.println("Could not establish connection to mariadb");
+            return;
+        }
+
+        Connection connection = optionalConnection.get();
+        long startTimestamp;
+        long endTimestamp;
+        try {
+            // create table
+            String createTableSql = "CREATE TABLE IF NOT EXISTS auth (name character varying(49), pubid character varying(149))";
+            Statement createTableStatementMariaDb = connection.createStatement();
+
+            createTableStatementMariaDb.execute(createTableSql);
+
+            createTableStatementMariaDb.close();
+
+            // insert method1 mariadb
+            startTimestamp = System.currentTimeMillis();
+            insertMethod1MariaDb(connection, inputFile);
+            endTimestamp = System.currentTimeMillis();
+
+            DbUtils.printTimestamps(startTimestamp, endTimestamp);
+            DbUtils.printCount(connection, "auth");
+            DbUtils.clearTable(connection, "auth");
+
+            // insert method2 mariadb
+            startTimestamp = System.currentTimeMillis();
+            insertMethod2MariaDb(connection, inputFile);
+            endTimestamp = System.currentTimeMillis();
+
+            DbUtils.printTimestamps(startTimestamp, endTimestamp);
+            DbUtils.printCount(connection, "auth");
+            DbUtils.clearTable(connection, "auth");
+
+            // insert method3 mariadb
+            startTimestamp = System.currentTimeMillis();
+            insertMethod3MariaDb(connection, inputFile);
+            endTimestamp = System.currentTimeMillis();
+
+            DbUtils.printTimestamps(startTimestamp, endTimestamp);
+            DbUtils.printCount(connection, "auth");
+            DbUtils.clearTable(connection, "auth");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static void insertMethod1(Connection connection, String inputFile) {
         System.out.println("--- Insert method1 ---");
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
@@ -87,10 +156,6 @@ public class Assignment1 {
             String line;
             PreparedStatement insertAuthorStatement = connection.prepareStatement("INSERT INTO public.auth (name, pubid) VALUES(?, ?)");
             while ((line = br.readLine()) != null) {
-                if ("\n".equals(line)) {
-                    continue;
-                }
-
                 String[] lineParts = line.split("\t");
 
                 if (lineParts.length != 2) {
@@ -128,6 +193,85 @@ public class Assignment1 {
             copyStatement.close();
         } catch (SQLException e) {
             System.err.println("Error while executing sql");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void insertMethod1MariaDb(Connection connection, String inputFile) {
+        System.out.println("--- Insert method1 MariaDB ---");
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            PreparedStatement insertAuthorStatement = connection.prepareStatement("INSERT INTO auth (name, pubid) VALUES(?, ?)");
+            while ((line = br.readLine()) != null) {
+                String[] lineParts = line.split("\t");
+
+                if (lineParts.length != 2) {
+                    System.err.println("Read line not formatted correctly, got: " + line);
+                    return;
+                }
+
+                insertAuthorStatement.setString(1, lineParts[0]);
+                insertAuthorStatement.setString(2, lineParts[1]);
+
+                insertAuthorStatement.execute();
+            }
+            insertAuthorStatement.close();
+        } catch (IOException e) {
+            System.err.println("Error reading from input file: " + e.getMessage());
+            return;
+        } catch (SQLException e) {
+            System.err.println("Error while executing sql");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void insertMethod2MariaDb(Connection connection, String inputFile) {
+        System.out.println("--- Insert method2 ---");
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            connection.setAutoCommit(false);
+            String line;
+            PreparedStatement insertAuthorStatement = connection.prepareStatement("INSERT INTO auth (name, pubid) VALUES(?, ?)");
+            while ((line = br.readLine()) != null) {
+                String[] lineParts = line.split("\t");
+
+                if (lineParts.length != 2) {
+                    System.err.println("Read line not formatted correctly, got: " + line);
+                    return;
+                }
+
+                insertAuthorStatement.setString(1, lineParts[0]);
+                insertAuthorStatement.setString(2, lineParts[1]);
+                insertAuthorStatement.addBatch();
+            }
+            insertAuthorStatement.executeBatch();
+            connection.commit();
+
+            insertAuthorStatement.close();
+
+            connection.setAutoCommit(true);
+        } catch (IOException e) {
+            System.err.println("Error reading from input file: " + e.getMessage());
+            return;
+        } catch (SQLException e) {
+            System.err.println("Error while executing sql");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void insertMethod3MariaDb(Connection connection, String inputFile) {
+        System.out.println("--- Insert method3 ---");
+        String loadSql = "LOAD DATA INFILE '" + "/tmp/" + inputFile + "' INTO TABLE auth " +
+                "FIELDS TERMINATED BY '\t' " +
+                "LINES TERMINATED BY '\n'";
+
+        try {
+            Statement loadStatement = connection.createStatement();
+
+            loadStatement.execute(loadSql);
+
+            loadStatement.close();
+        } catch (SQLException e) {
+            System.err.println("Error while executing sql: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
